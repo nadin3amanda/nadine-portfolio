86a37b479fee00ca888cb06a1514cf4816362eb5
nadin3amanda <nadine@soar.earth>
2023-08-01 21:40:43 +0800

    updates footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,27 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 import "../styles/Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <TwitterIcon />
         <FacebookIcon />
-        <LinkedInIcon />
+        <Link to="https://www.linkedin.com/in/nadine-amanda-ortega/">
+          <LinkedInIcon />
+        </Link>
+        <Link to="https://github.com/nadin3amanda">
+          <GitHubIcon />
+        </Link>
+        <Link to="https://twitter.com/nadin3amanda/">
+          <TwitterIcon />
+        </Link>
+        <Link to="https://instagram.com/nadineplays/">
+          <InstagramIcon />
+        </Link>
       </div>
       <p> &copy; 2022 nadine amanda</p>
     </div>

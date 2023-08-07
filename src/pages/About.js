dc8ea30f68e4d699ac014cff133b9012b22ec751
nadin3amanda <nadine@soar.earth>
2023-08-07 18:03:45 +0800

    updates about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,67 +1,147 @@
 import React from "react";
-import styled from "styled-components";
-import profile1 from "../assets/about/nadine_and_cat.jpg";
-import profile2 from "../assets/brand/ai-photo.jpg";
+import styled, { keyframes } from "styled-components";
+import profile1 from "../assets/brand/ai-photo.jpg";
+import profile2 from "../assets/about/nadine_and_cat.jpg";
 import profile3 from "../assets/about/nadine_as_sage.jpg";
 
 const AboutContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  width: 60vw;
+  margin: 0 auto;
+
+  @media only screen and (max-width: 600px) {
+    width: 100vw;
+  }
 `;
 
-const LeftColumn = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
-const RightColumn = styled.div`
-  flex: 1;
-  padding: 20px;
+const Header = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  width: 90%;
+
+  h1 {
+    font-size: 100px;
+    font-family: "Urbanist", sans-serif;
+  }
+  p {
+    margin-top: -3vw;
+    font-size: 30px;
+  }
+
+  @media only screen and (max-width: 600px) {
+    margin: 0 0 5% 3%;
+    align-items: center;
+    text-align: start;
+  }
+`;
+const Column = styled.div`
+  flex: 1;
+  padding: 2px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media only screen and (max-width: 600px) {
+    margin: 3px auto;
+  }
 `;
 
-const Description = styled.div`
+const ColumnInfo = styled.div`
+  width: 20vw;
+  height: 20vh;
+  text-align: start;
   font-size: 18px;
-  line-height: 1.6;
+  line-height: 1.7;
+  padding: 3px;
+  vertical-align: middle;
+  margin: 20px 0 20px 0;
+
+  @media only screen and (max-width: 600px) {
+    width: 90%;
+    margin: 10% 0 10% 0;
+  }
+`;
+
+const glowAnimation = keyframes`
+  0%, 100% {
+    border-color: #d16ba5;
+    box-shadow: 0 0 10px #d16ba5;
+  }
+  50% {
+    border-color: #5ffbf1;
+    box-shadow: 0 0 24px #5ffbf1;
+  }
 `;
 
 const Image = styled.img`
-  max-width: 100%;
-  height: auto;
-  border-radius: 8px;
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  border: 2px solid transparent;
+  box-shadow: 0 0 10px transparent;
+  animation: ${glowAnimation} 2s infinite;
+  margin: 5px auto;
+
+  @media only screen and (max-width: 600px) {
+    display: block;
+    margin: 0 auto;
+  }
 `;
 
 export default function About() {
   return (
     <AboutContainer>
-      <LeftColumn>
-        <Description>
+      <Header>
+        <h1>Hi! I'm Nadine.</h1>
+        <p>
           A creative Front End Developer with a passion for learning and
           designing.
-        </Description>
-      </LeftColumn>
-      <RightColumn>
-        <Image src={profile1} alt="Profile" />
-      </RightColumn>
-      <LeftColumn>
+        </p>
+      </Header>
+      <Column>
+        <ColumnInfo>
+          I am self-taught developer with over 6 years of experience in
+          building, designing, and maintaining websites, and a total work
+          experience of over a decade in multicultural settings. Always
+          passionate about learning something new and being able to provide
+          solutions to problems.
+        </ColumnInfo>
+        <Image src={profile1} alt="Profile" style={{ marginTop: "50px" }} />
+        <ColumnInfo>
+          I have a very unconventional work background which I am grateful for
+          because it contributed a lot to my work ethic. Commencing as Technical
+          Support Representative, I learned the art of communication and
+          adapting to a multicultural environment. As a Customer Insight
+          Consultant, I mastered quality assurance and the strategic use of
+          customer data. Progressing to an Executive Assistant, I engaged with
+          prestigious brands, all while nurturing my skills in Web Design and
+          Front-End Development.
+        </ColumnInfo>
+      </Column>
+      <Column>
         <Image src={profile2} alt="Profile" />
-      </LeftColumn>
-      <RightColumn>
-        <Description>
-          In my free time, I enjoy photography and capturing beautiful moments
-          with my camera.
-        </Description>
-      </RightColumn>
-      <LeftColumn>
-        <Description>
-          I have 5 years of experience working as a web developer. I have worked
-          on various projects and gained expertise in frontend technologies.
-        </Description>
-      </LeftColumn>
-      <RightColumn>
-        <Image src={profile3} alt="Work Experience" />
-      </RightColumn>
+        <ColumnInfo>
+          My skills that possibly be relevant for your projects are HTML/5,
+          CSS/3, SCSS, Javascript, Typescript, Jquery, React, NextJS, Styled
+          Components, Microsoft Visual Studio Code, Git, GitHub, Linux, Ubuntu,
+          iTerm 2, WordPress, Elementor, Microsoft Office, Google Suite, Adobe
+          Photoshop, Adobe Illustrator, Figma, Canva.
+        </ColumnInfo>
+        <Image
+          src={profile3}
+          alt="Work Experience"
+          style={{ marginTop: "50px", marginBottom: "50px" }}
+        />
+        <ColumnInfo>
+          Amidst this voyage, I maintained a steadfast dedication to my craft as
+          a Web Designer and Front-End Developer, a pursuit that has kept my
+          skills vibrant and nurtures a dream to make it a compass for my future
+          path. May my humble journey stand as a testament to the power of
+          growth, embracing the unexpected, and the unwavering dedication to the
+          artistry of progress.
+        </ColumnInfo>
+      </Column>
     </AboutContainer>
   );
 }

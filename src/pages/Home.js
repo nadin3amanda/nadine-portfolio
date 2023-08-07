bbf66245d059589072600d5b59e39b38caa58fc0
nadin3amanda <nadine@soar.earth>
2023-08-07 21:52:56 +0800

    remove unused

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,4 @@
 import React from "react";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import EmailIcon from "@material-ui/icons/Email";
-import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Home.css";
 
 function Home() {

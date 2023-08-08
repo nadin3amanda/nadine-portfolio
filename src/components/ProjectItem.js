edafd7b0bc709a8ca21480faa637cc8667e1cf6b
nadin3amanda <nadine@soar.earth>
2023-08-08 20:04:21 +0800

    Updates ProjectItem (hover + function)

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function ProjectItem({ image, name, id }) {
+function ProjectItem({ image, name, id, skills }) {
   const navigate = useNavigate();
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div
       className="projectItem"
-      onClick={() => {
-        navigate("/project/" + id);
-      }}
+      // onClick={() => {
+      //   navigate("/project/" + id);
+      // }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
       <h1> {name} </h1>
+      {isHovered && <div className="skills">{skills}</div>}
     </div>
   );
 }

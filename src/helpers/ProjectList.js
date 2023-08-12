642d0b9f542d3850bc91e27f44ed5d9dc0149440
nadin3amanda <nadine@soar.earth>
2023-08-12 10:46:33 +0800

    updates projectList with links

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -9,31 +9,37 @@ export const ProjectList = [
   {
     name: "Crypto App",
     image: Proj1,
-    skills: "React, TypeScript, CSS, Styled components",
+    skills: "React, TypeScript, CSS, Styled components, Coin Gecko API",
+    link: "https://crypto-app-rho-five.vercel.app/",
   },
   {
     name: "NextJs Blog Site",
     image: Proj2,
     skills: "Next, TypesScript",
+    link: "https://nextjs-blog-nadin3amanda.vercel.app/",
   },
   {
     name: "Pokedex",
     image: Proj3,
     skills: "React, JavaScript, Pokemon API",
+    link: "https://nadin3amanda.github.io/pokemon-app/",
   },
   {
     name: "New Artist Download Card",
     image: Proj4,
-    skills: "React,Node.js,MySQL,GraphQL",
+    skills: "HTML, CSS",
+    link: "https://github.com/nadin3amanda/Giane-Sales-Always-Been-You",
   },
   {
     name: "Gaming Stats Layout",
     image: Proj5,
     skills: "JavaScript,HTML,CSS",
+    link: "https://github.com/nadin3amanda/Gaming-Stat-Page",
   },
   {
     name: "Digital Atlas Landing Page",
     image: Proj6,
     skills: "React, React Carousel, TypeScript, HTML,CSS",
+    link: "https://soar.earth",
   },
 ];

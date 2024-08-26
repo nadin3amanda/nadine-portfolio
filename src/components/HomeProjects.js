9407cfc2bf72dc3098e50c1779814f0165f1f40b
nadine310 <nadine.ortega@310nutrition.com>
2024-08-26 16:56:43 +0800

    adds projects to home

--- src/components/HomeProjects.js
+++ src/components/HomeProjects.js
@@ -25,6 +25,34 @@ export default function HomeProjects() {
           <span>UI/UX, Interaction, Web Development</span>
         </div>
       </div>
+      <div id="project-container-2">
+        <Link to="/" target="_blank" rel="nofollow">
+          <img
+            id="itadakimasu-app"
+            src={
+              "https://live.staticflickr.com/65535/53948511181_888e9dc3ab_z.jpg"
+            }
+            alt="itadakimasu app"
+          />
+        </Link>
+        <div className="description">
+          <h6>Itadakimasu Food App</h6>
+          <span>UI/UX, Concept, Customer Journey, Prototype,</span>
+        </div>
+      </div>
+      <div id="project-container-3">
+        <img
+          id="reviewology"
+          src={
+            "https://live.staticflickr.com/65535/53948526846_6bf4df7e5f_z.jpg"
+          }
+          alt="reviewology blog -skin care"
+        />
+        <div className="description">
+          <h6>Reviewology - Blog SIte</h6>
+          <span>UI/UX, Web Development</span>
+        </div>
+      </div>
       <Link to="/projects">
         <button className="more-button">More</button>
       </Link>

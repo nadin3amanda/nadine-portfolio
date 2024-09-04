e42c40d0cefa6c3e8bdc8e99188c66cad165b98e
nadine310 <nadine@soar.earth>
2024-09-04 10:01:46 +0800

    fixes home route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ThemeProvider } from "../src/components/ThemeProvider";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
-import Navbar from "./components/Navbar";
+import "./App.css";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
 import ProjectDisplay from "./pages/ProjectDisplay";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <ThemeProvider>
       <div className="h-screen flex justify-center items-center">
         <div className="App">
-          <Router>
+          <Router basename="/nadine-portfolio">
             <Navbar />
             <Routes>
-              <Route path="" element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="/projects" element={<Projects />} />
               <Route path="/project/:id" element={<ProjectDisplay />} />
               <Route path="/about" element={<About />} />
